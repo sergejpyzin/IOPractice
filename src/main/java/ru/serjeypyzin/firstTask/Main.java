4929aa539e303312ceff6aff02ac0be3a2334ff8
Sergej Pyzin <sergejpyzin@yandex.ru>
2024-01-18 09:45:08 +0300

    Created firstTask

---
+++ src/main/java/ru/serjeypyzin/firstTask/Main.java
@@ -0,0 +1,33 @@
+package ru.serjeypyzin.firstTask;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        backupFiles(".");
+    }
+
+    public static void backupFiles (String path){
+        File currentDirectory = new File(path);
+        String pathToBackup = path + "/backup";
+        File backupDirectory = new File(pathToBackup);
+
+        if (!backupDirectory.exists()){
+            backupDirectory.mkdir();
+        }
+
+        Arrays.stream(currentDirectory.listFiles())
+                .filter(File::isFile)
+                .forEach(file -> {
+                    try {
+                        Files.copy(file.toPath(), Path.of(pathToBackup, file.getName()));
+                    } catch (IOException e){
+                        throw new RuntimeException();
+                    }
+                });
+    }
+}
